ad4f7fae80dfae6045b12e0ebaf14dfb1d62c798
SoftFusion - Technologies S.A. <153782422+SoftFusion-Technologies@users.noreply.github.com>
2024-05-25 05:46:57 +0000

    newvite

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-      rollupOptions: {
-        external: ['html2canvas']
-      }
+    rollupOptions: {
+      external: ['html2canvas']
     }
+  }
 })
 
